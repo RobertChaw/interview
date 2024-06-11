1636db6f3cabed12ee0e35f854dcaff8a68083a5
Robert Chaw <1335952506@qq.com>
2024-06-11 20:26:44 +0800

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+} from "@nextui-org/react";
+
+export default function Home() {
+  return (
+    <main className="">
+      <div className={"font-bold text-2"}>Hello world</div>
+    </main>
+  );
+}
